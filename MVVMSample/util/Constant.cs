98c5e2610f0fac9c9fb50aa02034214c7305391f
yasumo <ndve0027@gmail.com>
2016-12-26 18:16:25 +0900

    Behaviorでシステムコマンドをハンドリング、最小化や閉じるボタンの無効化

---
+++ MVVMSample/util/Constant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.util
+{
+    public class Constant
+    {
+        //--- GetWindowLong
+        public const int GWL_STYLE = -16;
+        public const int GWL_EXSTYLE = -20;
+        //--- Window Style
+        public const int WS_EX_CONTEXTHELP = 0x00400;
+        public const int WS_MAXIMIZEBOX = 0x10000;
+        public const int WS_MINIMIZEBOX = 0x20000;
+        public const int WS_SYSMENU = 0x80000;
+        //--- Window Message
+        public const int WM_SYSKEYDOWN = 0x0104;
+        public const int WM_SYSCOMMAND = 0x0112;
+        //--- System Command
+        public const int SC_CONTEXTHELP = 0xF180;
+        public const int SC_CLOSE = 0xF060;
+        //--- Virtual Keyboard
+        public const int VK_F4 = 0x73;
+        //--- Constructor
+        private Constant() { }
+    }
+}
---
+++ MVVMSample/util/User32.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMSample.util
+{
+    public static class User32
+    {
+        [DllImport("user32.dll")]
+        public extern static int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        public extern static int SetWindowLong(IntPtr hwnd, int index, int value);
+    }
+}
